cdf4f8c228ee6bdbace9f4e26544a0e9e6f433c4
Agustin <agustin@edsurge.com>
2019-03-25 13:33:43 -0700

    liking a tweet

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,7 +1,18 @@
-import { RECEIVE_TWEETS } from '../actions/tweets'
+import { RECEIVE_TWEETS, TOGGLE_TWEET } from '../actions/tweets'
 
 export default function tweets (state = {}, action) {
   switch(action.type) {
+    case TOGGLE_TWEET :
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          likes: action.hasLiked === true
+            ? state[action.id].likes.filter((uid) => uid !== action.authedUser)
+            : state[action.id].likes.concat([action.authedUser])
+        }
+
+      }
     case RECEIVE_TWEETS :
       return {
                 ...state,
